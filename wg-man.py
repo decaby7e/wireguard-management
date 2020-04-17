20470a107d57f861e681fdb8e78a91f29f477a21
decaby7e <27081624+decaby7e@users.noreply.github.com>
2020-04-16 21:24:35 -0400

    🔧 Updates

--- wg-man.py
+++ wg-man.py
@@ -2,20 +2,38 @@
 
 import yaml
 import subprocess
+import wireguard.py
 
 class Client():
-    
+    def __init__(self):
+        self.name
+        self.ip
+        self.pubkey = subprocess.call("")
+        self.privkey = subprocess.call(["wg", "genkey"])
+        
+        proc = subprocess.Popen(['cdrecord', '--help'], stdout=subprocess.PIPE)
+        
+        self.server_ip
+        self.server_port
+        self.server_pubkey
+
+        def gen_conf(self):
+            subprocess.call(["./gen-wg-client.sh", "-s", server_pubkey,
+                            "-a", server_ip, "-p", server_port, "-c", ip,
+                            "-f", "{}.conf".format(name), "-n", name])
+            return "{}.conf".format(name)
 
 class Server():
 
 class Network():
     # Public variables go outside init
     
-    def __init__(self):
+    def __init__(self, conf_file):
         self.name
         self.subnet
         self.version
         self.dns
+        self.yaml = yaml.load(conf_file)
 
     def add_server(self):
         # Create server in YAML and return True on success
@@ -33,6 +51,7 @@ class Network():
     def del_client(self):
 
 def main():
+    Network n = Network(open('prototype.yaml'))
 
 if __name__=='__main__':
     main()
